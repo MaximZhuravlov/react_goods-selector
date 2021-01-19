61eba05b666c8182ab961f11cd387af0a3301d50
Maxim Zhuravlyov <maksym.zhuravlov1995@gmail.com>
2021-01-19 13:39:51 +0200

    Updates

---
+++ precache-manifest.e33c572db197c7597a8970c4f8f2ef39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af2d00a87ccd7098779b16ecc080934",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0635b8dd8006b59e7c0d",
+    "url": "./static/css/main.a333e086.chunk.css"
+  },
+  {
+    "revision": "423d1f8f07466d4c3b37",
+    "url": "./static/js/2.0704ab71.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.0704ab71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0635b8dd8006b59e7c0d",
+    "url": "./static/js/main.4dae7c42.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
